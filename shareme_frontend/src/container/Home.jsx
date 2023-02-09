3ae9faf5b32f2f4d68e301136e8aceb320098be7
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2023-02-09 17:44:10 +0200

    implement navbar component

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
             <img src={logo} alt="logo" className='w-28'/>
           </Link>
           <Link to={`user-profile/${user?._id}`}>
-            <img src={user?.image} alt="logo" className='w-28' referrerpolicy="no-referrer"/>
+            <img src={user?.image} alt="logo" className='w-28' referrerPolicy="no-referrer"/>
           </Link>
         </div>
       {toggleSidebar && (
